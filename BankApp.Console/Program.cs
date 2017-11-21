16a4be5d98f00882b9848398adb0b705b23a3687
adsieg01 <adsieg01@gmail.com>
2017-11-21 17:00:52 -0500

    Created initial interface

--- BankApp.Console/Program.cs
+++ BankApp.Console/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Bank.Services;
+using Figgle;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BankApp.Console
@@ -11,9 +13,48 @@ namespace BankApp.Console
     {
         static void Main(string[] args)
         {
-            CustomerServices bank = new CustomerServices();
-            bank.CreateCustomer("Jack", "Sparrow", 7);
-        
+
+            System.Console.WriteLine(FiggleFonts.Standard.Render("Bank of Tomorrow"));
+            Thread.Sleep(1000);
+            System.Console.WriteLine(FiggleFonts.Standard.Render("Welcome"));
+            Thread.Sleep(3000);
+            System.Console.WriteLine("Please Press Enter 2X To Begin");
+            System.ConsoleKeyInfo keyInfo = System.Console.ReadKey(true);
+            
+            //Allows user to proceed only after hitting Enter twice
+            do
+            {
+               keyInfo = System.Console.ReadKey(true);
+                if (keyInfo.Key != ConsoleKey.Enter)
+                {                  
+                    System.Console.WriteLine("Please Press Enter 2X To Begin");
+                }
+            }
+            while (keyInfo.Key != ConsoleKey.Enter);
+            System.Console.Clear();
+
+            //Menu of options
+            System.Console.WriteLine(FiggleFonts.Standard.Render("ATM"));
+            System.Console.WriteLine("\nMain Menu\n" +
+               "1: Login\n" +
+               "2: Create Account\n" +
+               "3: Exit\n"); 
+            int inputOpt = Int32.Parse(System.Console.ReadLine());
+
+            //Create Account
+            //Exit
+
+
+            //Login
+            System.Console.WriteLine(FiggleFonts.Standard.Render("ATM"));
+            System.Console.WriteLine("Enter name and PIN number");
+            //Reference Database to see if user info is vaild
+            string userName = System.Console.ReadLine();
+            System.Console.WriteLine(userName);
+
+            System.Console.ReadLine();
+
+
 
         }
     }
