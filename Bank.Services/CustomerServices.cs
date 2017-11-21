a5751229696950acc3448674fb3cd0805af334b6
adsieg01 <adsieg01@gmail.com>
2017-11-21 13:17:51 -0500

    first commit

---
+++ Bank.Services/CustomerServices.cs
@@ -0,0 +1,30 @@
+﻿using Bank.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Services
+{
+    public class CustomerServices
+    {
+       public bool CreateCustomer(string first, string last, int ID)
+        {
+            using (var context = new Bank_AppEntities())
+            {
+                var newUser = new Customer
+                {
+                    CustomerID = ID,
+                    FirstName = first,
+                    LastName = last
+                };
+                context.Customers.Add(newUser);
+
+                return context.SaveChanges() == 1;
+            }
+        }
+
+
+    }
+}
