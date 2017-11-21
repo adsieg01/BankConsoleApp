faf044920fdd5d930a88ab57fa48673f562abd9d
adsieg01 <adsieg01@gmail.com>
2017-11-21 13:45:07 -0500

    Inserted connection strings, tested add customer function

--- BankApp.Console/Program.cs
+++ BankApp.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bank.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,9 @@ namespace BankApp.Console
     {
         static void Main(string[] args)
         {
-            new CreateCustomer
+            CustomerServices bank = new CustomerServices();
+            bank.CreateCustomer("Jack", "Sparrow", 7);
+        
 
         }
     }
