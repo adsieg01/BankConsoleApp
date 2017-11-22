1445fc8bbec0552e7c1fc297ddcbd70c5e5e2af2
adsieg01 <adsieg01@gmail.com>
2017-11-22 11:40:04 -0500

    Started adding login validation

---
+++ Bank.Services/AuthServices.cs
@@ -0,0 +1,34 @@
+﻿using Bank.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Services
+{
+    class AuthServices
+    {
+        void login() {
+
+            var db = new Bank_AppEntities();
+
+            User user = (from u in db.Customers
+                         where u.FirstName.Equals(firstname) &&
+                         u.LastName.Equals(lastname) &&
+                         from p in db.Accounts
+                         where p.PIN.Equals(pin)
+                         select p).FirstOrDefault();
+
+            if (user == null)
+            {
+                //Invalid 
+            }
+            else
+            {
+                // Success
+            }
+            
+                }
+    }
+}
--- BankApp.Console/Program.cs
+++ BankApp.Console/Program.cs
@@ -13,45 +13,50 @@ namespace BankApp.Console
     {
         static void Main(string[] args)
         {
-
-            System.Console.WriteLine(FiggleFonts.Standard.Render("Bank of Tomorrow"));
-            Thread.Sleep(1000);
-            System.Console.WriteLine(FiggleFonts.Standard.Render("Welcome"));
-            Thread.Sleep(3000);
-            System.Console.WriteLine("Please Press Enter 2X To Begin");
-            System.ConsoleKeyInfo keyInfo = System.Console.ReadKey(true);
+            //Opening Screen
+                System.Console.WriteLine(FiggleFonts.Standard.Render("Bank of Tomorrow"));
+                Thread.Sleep(1000);
+                System.Console.WriteLine(FiggleFonts.Standard.Render("Welcome"));
+                Thread.Sleep(3000);
+                System.Console.WriteLine("Please Press Enter 2X To Begin");
+                System.ConsoleKeyInfo keyInfo = System.Console.ReadKey(true);
             
             //Allows user to proceed only after hitting Enter twice
-            do
-            {
-               keyInfo = System.Console.ReadKey(true);
-                if (keyInfo.Key != ConsoleKey.Enter)
-                {                  
-                    System.Console.WriteLine("Please Press Enter 2X To Begin");
+                do
+                {
+                   keyInfo = System.Console.ReadKey(true);
+                    if (keyInfo.Key != ConsoleKey.Enter)
+                    {                  
+                        System.Console.WriteLine("Please Press Enter 2X To Begin");
+                    }
                 }
-            }
-            while (keyInfo.Key != ConsoleKey.Enter);
-            System.Console.Clear();
+                while (keyInfo.Key != ConsoleKey.Enter);
+                System.Console.Clear();
 
             //Menu of options
-            System.Console.WriteLine(FiggleFonts.Standard.Render("ATM"));
-            System.Console.WriteLine("\nMain Menu\n" +
-               "1: Login\n" +
-               "2: Create Account\n" +
-               "3: Exit\n"); 
-            int inputOpt = Int32.Parse(System.Console.ReadLine());
-
-            //Create Account
-            //Exit
-
+                System.Console.WriteLine(FiggleFonts.Standard.Render("ATM"));
+                System.Console.WriteLine("\nMain Menu\n" +
+                   "1: Login\n" +
+                   "2: Create Account\n" +
+                   "3: Exit\n"); 
+                int inputOpt = Int32.Parse(System.Console.ReadLine());
 
             //Login
-            System.Console.WriteLine(FiggleFonts.Standard.Render("ATM"));
-            System.Console.WriteLine("Enter name and PIN number");
-            //Reference Database to see if user info is vaild
-            string userName = System.Console.ReadLine();
-            System.Console.WriteLine(userName);
+            if(inputOpt == 1) { 
+                System.Console.Clear();
+                System.Console.WriteLine(FiggleFonts.Standard.Render("ATM"));
+                System.Console.WriteLine("Enter name and PIN number");
+                //Reference Database to see if user info is vaild
+                string userName = System.Console.ReadLine();
+                System.Console.WriteLine(userName);
+                }
 
+
+            //Create Account
+                //Connect Database
+
+            //Exit
+                //Break the application 
             System.Console.ReadLine();
 
 
